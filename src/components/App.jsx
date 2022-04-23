4a3d16e1f0bc35689ae1227705d1c861d1e75546
MaksymChuraiev <mchuraev1988@gmail.com>
2022-04-23 09:49:12 +0300

    Add files

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+// import { ToastContainer } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import * as Scroll from 'react-scroll';
+
+export const App = () => {
+  return <div>React template</div>;
+};
